1d660099760aec00861140943d71648d1277411f
adauby <adaubyassi@gmail.com>
2021-04-20 18:28:47 +0000

    first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'harvest-items';
+  title = 'Moisson 2021';
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  title : string = 'Moisson 2021';
+  description : string = 'Liste des articles de la deuxieme mini moisson'
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
